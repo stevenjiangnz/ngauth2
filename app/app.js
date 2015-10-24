495d932940aaee8fb0d40bcae44e961382cea273
Steven <sj@jnetsolution.com.au>
2015-10-25 07:08:07 +1000

    include map file

---
+++ app/app.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Stevenj on 25/10/2015.
+ */
+var myclass = (function () {
+    function myclass(name) {
+        this.name = name;
+    }
+    return myclass;
+})();
+//# sourceMappingURL=app.js.map
